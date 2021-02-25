4f1e1888c44f9d691a751204febc0e61e684d952
Hemant Kumar <63533985+Hemant1101@users.noreply.github.com>
2021-02-25 23:16:32 +0530

    Update binarytree.c
    added level order traversal

--- binarytree.c
+++ binarytree.c
@@ -12,7 +12,45 @@ struct binarytree
     struct binarytree *right;
 };
 
-// creattion of binary tree
+struct qnode
+{
+    struct binarytree *data;
+    struct qnode *next;
+};
+
+struct queue
+{
+    struct qnode *rear;
+    struct qnode *front;
+};
+
+struct binarytree *create();
+void preorder(struct binarytree *);
+void inorder(struct binarytree *);
+void postorder(struct binarytree *);
+void levelorder(struct binarytree *);
+
+struct queue *createqueue();
+int isemptyqueue(struct queue *);
+struct binarytree *dequeue(struct queue *);
+void enqueue(struct queue *, struct binarytree *);
+void deletequeue(struct queue *);
+
+int main()
+{
+    struct binarytree *root;
+    root = create();
+    printf("\ninorder traversal of tree:\n");
+    inorder(root);
+    printf("\npreorder traversal of tree:\n");
+    preorder(root);
+    printf("\npostorder traversal of tree:\n");
+    postorder(root);
+    printf("\nlevelorder traversal of tree:\n");
+    levelorder(root);
+    return 0;
+}
+
 struct binarytree *create()
 {
     struct binarytree *newnode;
@@ -30,7 +68,6 @@ struct binarytree *create()
     return newnode;
 }
 
-// preorder travarsal
 void preorder(struct binarytree *root)
 {
     if (root == NULL)
@@ -42,7 +79,6 @@ void preorder(struct binarytree *root)
     preorder(root->right);
 }
 
-// inorder travarsal
 void inorder(struct binarytree *root)
 {
     if (root == NULL)
@@ -54,7 +90,6 @@ void inorder(struct binarytree *root)
     inorder(root->right);
 }
 
-// postorder travarsal
 void postorder(struct binarytree *root)
 {
     if (root == NULL)
@@ -66,15 +101,88 @@ void postorder(struct binarytree *root)
     printf("%d ", root->data);
 }
 
-int main()
+void levelorder(struct binarytree *root)
 {
-    struct binarytree *root;
-    root = create();
-    printf("\ninorder traversal of tree:\n");
-    inorder(root);
-    printf("\npreorder traversal of tree:\n");
-    preorder(root);
-    printf("\npostorder traversal of tree:\n");
-    postorder(root);
-    return 0;
+    struct binarytree *temp;
+    struct queue *q = createqueue();
+    if (!root)
+    {
+        return;
+    }
+    enqueue(q, root);
+    while (!isemptyqueue(q))
+    {
+        temp = dequeue(q);
+        printf("%d ", temp->data);
+        if (temp->left)
+        {
+            enqueue(q, temp->left);
+        }
+        if (temp->right)
+        {
+            enqueue(q, temp->right);
+        }
+    }
+    deletequeue(q);
+}
+
+struct qnode *newnode(struct binarytree *ptr)
+{
+    struct qnode *temp = (struct qnode *)malloc(sizeof(struct qnode));
+    temp->data = ptr;
+    temp->next = NULL;
+    return temp;
+}
+
+struct queue *createqueue()
+{
+    struct queue *q = (struct queue *)malloc(sizeof(struct queue));
+    q->front = q->rear = NULL;
+    return q;
+}
+
+int isemptyqueue(struct queue *q)
+{
+    if (q->front == NULL)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+struct binarytree *dequeue(struct queue *q)
+{
+    if (q->front == NULL)
+    {
+        return NULL;
+    }
+    struct qnode *temp = q->front;
+    q->front = q->front->next;
+    if (q->front == NULL)
+    {
+        q->rear = NULL;
+    }
+    struct binarytree *ptr = temp->data;
+    free(temp);
+    return ptr;
+}
+
+void enqueue(struct queue *q, struct binarytree *root)
+{
+    struct qnode *temp = newnode(root);
+    if (q->rear == NULL)
+    {
+        q->front = q->rear = temp;
+        return;
+    }
+    q->rear->next = temp;
+    q->rear = temp;
+}
+
+void deletequeue(struct queue *q)
+{
+    free(q);
 }
